c4ce1b3c6e8ab86ce5860ca9385f9849eac2176b
Peter Donald <peter@realityforge.org>
2010-07-01 23:24:11 +1000

    Add a logger that displays no message at the end of build

---
+++ src/main/java/org/realityforge/antix/BasicLogger.java
@@ -0,0 +1,12 @@
+package org.realityforge.antix;
+
+import org.apache.tools.ant.BuildEvent;
+import org.apache.tools.ant.NoBannerLogger;
+
+public class BasicLogger
+  extends NoBannerLogger
+{
+  public void buildFinished( final BuildEvent event )
+  {
+  }
+}
