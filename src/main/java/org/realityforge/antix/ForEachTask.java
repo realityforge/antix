e4466994440427cd6ca7f20272befa83330523ec
Peter Donald <peter@realityforge.org>
2018-01-10 11:53:21 +1100

    Codestyle

--- src/main/java/org/realityforge/antix/ForEachTask.java
+++ src/main/java/org/realityforge/antix/ForEachTask.java
@@ -40,8 +40,10 @@ public class ForEachTask
       final MacroDef.Attribute attribute = new MacroDef.Attribute();
       attribute.setName( property );
       macroDef.addConfiguredAttribute( attribute );
-      if (values.isEmpty())
+      if ( values.isEmpty() )
+      {
          return;
+      }
       final String[] values = list.split( " " );
       for ( final String value : values )
       {
